a0910da83bb527fbee71b0cb042c1e60f929a433
Jana Marie Riess <j.riess@wtnet.de>
2022-03-23 14:26:45 +0100

    work on p fits

--- two_fields/coupled_fields.jl
+++ two_fields/coupled_fields.jl
@@ -9,12 +9,23 @@ using DelimitedFiles
 using Random
 using Roots
 
+############################### background cosmology #########################
+const p = 2
+const t0 = 0.0
+const a0 = 1.0
+
+H_to_t(H, H0) = 1/p*(1/H - 1/H0) + t0
+t_to_a(t, H0) = a0 * (p*H0*(t - t0) + 1)^(1/p)
+a_to_t(a, H0) = 1/(p*H0)*((a/a0)^p - 1) + t0
+H_to_a(H, H0) = a0 * (H0 / H)^(1/p)
+
 ################################ the model ##################################
-function coupled_fields_rhs(s, p, t)
+
+function coupled_fields_rhs(s, params, t)
     H, phi1, phi1_dot, phi2, phi2_dot = s
-    M, G = p
+    M, G = params
     return SVector(
-        -2*H^2,
+        -p*H^2,
         phi1_dot,
         - 3*H*phi1_dot - G*phi1*phi2^2 - phi1,
         phi2_dot,
@@ -22,37 +33,38 @@ function coupled_fields_rhs(s, p, t)
     )
 end
 
-function coupled_fields(M, G, initial_ratio, H_start)
-    initial = [H_start, 1.0, 0.0, initial_ratio, 0.0]
-    return ContinuousDynamicalSystem(coupled_fields_rhs, initial, [M, G])
+function coupled_fields(M, G, initial_ratio, H0)
+    initial = [H0, 1.0, 0.0, initial_ratio, 0.0]
+    return ContinuousDynamicalSystem(coupled_fields_rhs, initial, [M, G], t0=t0)
 end
 
 calc_pot(M, G, phi1, phi2) = G*phi1^2*phi2^2 + 0.5*phi1^2 + 0.5*M*phi2^2
 
-function calc_energy(sys, orbit)
+function calc_energy(sys, sol)
     M, G = sys.p
-    H, phi1, phi1_dot, phi2, phi2_dot = columns(orbit)
+    @views H, phi1, phi1_dot, phi2, phi2_dot = sol[1,:], sol[2,:], sol[3,:], sol[4,:], sol[5,:]
     return @. 0.5*phi1_dot^2 + 0.5*M*phi2_dot^2 + calc_pot.(M, G, phi1, phi2)
 end
 
-function calc_eff_masses(sys, orbit)
-    return @.(orbit[:,4]^2 + 1), @.(orbit[:,2]^2 + sys.p[1])
-end
+calc_eff_masses(sys, sol) = @.(sol[4,:]^2 + 1), @.(sol[2,:]^2 + sys.p[1])
 
 model(x, p) = @. p[1] * x + p[2]
-H_to_t(H, H_start) = 0.5*(1/H - 1/H_start)
 
 const alg = AutoTsit5(Rosenbrock23())
 const settings = (reltol=1e-6, abstol=1e-6)
 
+G_range = (10 .^ (-2:0.2:6))
+initial_ratio_range = [1, 1 + 1e-2, 10, 100]
+M_range = [1e-2, 1e-1, 1, 10, 100]
+
 ######################## plot trajectories and phasespace projections ###################
-function plot_evolution(M, G, initial_ratio, H_start; nsteps=1000, H_end=M/3)
-    tmax = H_to_t(H_end, H_start)
-    t0 = H_to_t(H_start / 1.001, H_start)
+function plot_evolution(M, G, initial_ratio, H0; nsteps=1000, H_end=M/3)
+    tmax = H_to_t(H_end, H0)
+    t0 = H_to_t(H0 / 1.001, H0)
     ts = [0.0; exp.(range(log(t0), log(tmax), length=nsteps - 1))]
-    problem = ODEProblem(coupled_fields(M, G, initial_ratio, H_start) , (0, tmax))
+    problem = ODEProblem(coupled_fields(M, G, initial_ratio, H0) , (0, tmax))
     sol = solve(problem, alg; settings..., saveat=ts)
-    H, phi1, phi1_dot, phi2, phi2_dot = sol[1,:], sol[2,:], sol[3,:], sol[4,:], sol[5,:]
+    @views H, phi1, phi1_dot, phi2, phi2_dot = sol[1,:], sol[2,:], sol[3,:], sol[4,:], sol[5,:]
     n = 100
     a = minimum(phi1)
     b = maximum(phi1)
@@ -79,72 +91,10 @@ function plot_evolution(M, G, initial_ratio, H_start; nsteps=1000, H_end=M/3)
     plot(phi1, phi2, color="red")
     xlabel(raw"$\phi_1$")
     ylabel(raw"$\phi_2$")
-    suptitle("M = $M, G = $G, \$\\phi_1 / \\phi_2\$ = $initial_ratio, \$H_0\$ = $H_start")
+    suptitle("M = $M, G = $G, \$\\phi_1 / \\phi_2\$ = $initial_ratio, \$H_0\$ = $H0")
     tight_layout()
 end
 
-
-############################## analysis of the energy scaling ##########################
-function compute_p(M, G;
-        debug=false, nsamples=1, window=4, nsteps=200, initial_ratio=1 + 1e-2,
-        H_start=1e2*M, H_fit_start=1e-1*M, H_end=1e-2*M,
-        alg=SimpleATsit5(), reltol=1e-6, abstol=1e-6)
-    @show (G, M, initial_ratio, H_start)
-    Random.seed!(42)
-    tmax = H_to_t(H_end, H_start)
-    t_fit_start = H_to_t(H_fit_start, H_start)
-    log_t_start = -5
-    log_ts = range(log_t_start, log(tmax), length=nsteps)
-    d_log_t = log_ts[2] - log_ts[1]
-    ts = exp.(log_ts)
-    i = ceil(Int, (log(t_fit_start) - log_t_start) / d_log_t + 1) # t = e^(log(t_start) + (i - 1) * d_log_t)
-    x = @view(log_ts[i:end])
-    _smooth_x = [mean(x[k-window:k+window]) for k = 1 + window : length(x) - window]
-    ps = Float64[]
-    p_errs = Float64[]
-    param_guess = [1.0, 1.0]
-    @time for k = 1:nsamples
-        sys = coupled_fields(M, G, initial_ratio, H_start)
-        problem = ODEProblem(sys, (0.0, tmax))
-        sol = solve(problem, alg; saveat=ts, abstol=abstol, reltol=reltol)
-        orbit = Dataset(collect(sol.u))
-        energy = calc_energy(sys, orbit)
-        y = log.(@view(energy[i:end]))
-        smooth_y = [mean(y[k-window:k+window]) for k = 1 + window : length(x) - window]
-        mask = isfinite.(smooth_y)
-        smooth_x = _smooth_x[mask]
-        smooth_y = smooth_y[mask]
-        fit_res = curve_fit(model, smooth_x, smooth_y, param_guess)
-        param_guess = fit_res.param
-        p = 2*fit_res.param[1]
-        p_err = 2*sqrt(estimate_covar(fit_res)[1, 1])
-        push!(ps, p)
-        push!(p_errs, p_err)
-        if debug
-            plot(log.(ts), log.(energy))
-    title("M = $M, G = $G, \$\\phi_1 / \\phi_2\$ = $initial_ratio, \$H_0\$ = $H0")
-            axvline(x[1], color="black")
-            plot(smooth_x, smooth_y)
-            plot(smooth_x, model(smooth_x, fit_res.param))
-        end
-        initial_ratio = 1 + 1e-1*(2*rand() - 1)
-    end
-    p_err = max(sqrt(sum(p_errs.^2))/length(p_errs), nsamples == 1 ? -1 : std(ps))
-    p = mean(ps)
-    println("p = $p +/- $p_err")
-    return p, p_err
-end
-
-function plot_p_fits(G_range, p_list)
-    errorbar(G_range, [p[1] for p in p_list], yerr=[p[2] for p in p_list], capsize=3.0, fmt="x")
-    xscale("log")
-    xlabel("G")
-    ylabel("p")
-end
-
-function save_p_fits(G_range, p_list; filename="p_fits.dat")
-    writedlm(filename, [G_range [p[1] for p in p_list] [p[2] for p in p_list]])
-end
 
 ########################################## chaos tests ####################################
 function lya_convergence(; M=1.0, G=1e6, initial_ratio=1 + 1e-2, H0=1e3, nsteps=50)
@@ -227,8 +177,9 @@ function _find_H_osc(M, G, initial_ratio, H0, Hmax, divisions)
 end
 
 function find_H_osc(M, G, initial_ratio;
-        H0_start=1e2*M, Hmax_start=M/4, nmaxtrys=100, divisions=1000, inc_factor=5, check_factor=5, epsilon=1e-2)
-    Hmax = Hmax_start
+        H0_start=1e2*M, Hmax_start=M/4, nmaxtrys=100, divisions=1000,
+        inc_factor=5, check_factor=5, epsilon=1e-2)
+     Hmax = Hmax_start
     H0 = H0_start
     for i = 1:nmaxtrys
         H_osc = _find_H_osc(M, G, initial_ratio, H0, Hmax, divisions)
@@ -254,9 +205,6 @@ function find_H_osc(M, G, initial_ratio;
     return NaN
 end
 
-G_range = (10 .^ (-2:0.2:6))
-initial_ratio_range = [1, 1 + 1e-2, 10, 100]
-M_range = [1e-2, 1e-1, 1, 10, 100]
 H_osc_filename(M, initial_ratio) = "coupled_fields_H_osc_M=$(M)_initial_ratio=$(initial_ratio).dat"
 
 function compute_H_osc()
@@ -268,32 +216,119 @@ function compute_H_osc()
     end
 end
 
-function plot_H_osc()
+function calc_H_osc_analytical(M, G, initial_ratio, n)
+    return n * max(sqrt(1 + G*initial_ratio^2), sqrt(M + G))
+end
+
+function plot_H_osc(;n=2)
     for (i, initial_ratio) in enumerate(initial_ratio_range)
         subplot(2, 2, i)
         title(@sprintf("\$\\phi_1 / \\phi_2\$ = %.2f", initial_ratio))
-        for M in M_range
+        for (j, M) in enumerate(M_range)
             try
                 data = readdlm(H_osc_filename(M, initial_ratio))
                 G_range, H_osc_list = data[:, 1], data[:, 2]
-                l = plot(G_range, abs.(H_osc_list), label=@sprintf("M = %.2f", M))
-                # plot(G_range, H_osc_list2, color=l[1].get_color(), ls="--")
-                xscale("log")
-                yscale("log")
-                xlabel("G")
-                ylabel(raw"$H_\mathrm{osc}$")
-                legend()
+                l = plot(G_range, abs.(H_osc_list), label=@sprintf("simulation M = %.2f", M))
+                H_osc_analytical = calc_H_osc_analytical.(M, G_range, initial_ratio, n)
+                plot(G_range, H_osc_analytical, color=l[1].get_color(),
+                     label=j == 1 ? "analytical" : nothing, ls="--")
             catch
             end
+        end
+        xscale("log")
+        yscale("log")
+        xlabel("G")
+        ylabel(raw"$H_\mathrm{osc}$")
+        if i == 1
+            legend()
         end
     end
     tight_layout()
 end
 
+############################## analysis of the energy scaling ##########################
+function find_p(M, G; debug=false, nsamples=1, window=4, nsteps=200)
+    @show (G, M)
+    Random.seed!(42)
+    ps = Float64[]
+    p_errs = Float64[]
+    param_guess = [1.0, 1.0]
+    @time for k = 1:nsamples
+        # random initial condition setup
+        initial_ratio = 1 + 1e-1*(2*rand() - 1)
+        println("sample: $k, initial_ratio: $initial_ratio")
+        # hubble scales
+        H0 = calc_H_osc_analytical(M, G, initial_ratio, 1/10)
+        H_fit_start = H0 / 10
+        H_end =  H0 / 1000
+        # log spaced a's
+        a_range = exp.(range(log(H_to_a(H0, H0)), log(H_to_a(H_end, H0)), length=nsteps))
+        # time steps
+        ts = a_to_t.(a_range, H0)
+        # fitting range
+        t_fit_start = H_to_t(H_fit_start, H0)
+        fit_start_index = findfirst(t -> t >= t_fit_start, ts)
+        @assert fit_start_index != nothing
+        # solve for the evolution
+        sys = coupled_fields(M, G, initial_ratio, H0)
+        problem = ODEProblem(sys, (ts[1], ts[end]))
+        sol = solve(problem, alg; saveat=ts, settings...)
+        # energy to fit
+        energy = calc_energy(sys, sol)
+        # prepare fitting data
+        @views smooth_y = [mean(log.(energy[k-window:k+window])) for k = fit_start_index + window : nsteps - window]
+        @views smooth_x = [mean(log.(a_range[k-window:k+window])) for k = fit_start_index + window : nsteps - window]
+        # FIXME: skip checking for invalid data for now ---> seems to be okay
+        # do the fit
+        fit_res = curve_fit(model, smooth_x, smooth_y, param_guess)
+        param_guess = fit_res.param
+        p = fit_res.param[1]
+        p_err = 2*sqrt(estimate_covar(fit_res)[1, 1])
+        push!(ps, p)
+        push!(p_errs, p_err)
+        # make debug plot
+        if debug
+            plot(log.(a_range), log.(energy))
+            axvline(smooth_x[1], color="black")
+            plot(smooth_x, smooth_y)
+            plot(smooth_x, model(smooth_x, fit_res.param))
+        end
+    end
+    p_err = max(sqrt(sum(p_errs.^2))/length(p_errs), nsamples == 1 ? -1 : std(ps))
+    p = mean(ps)
+    println("p = $p +/- $p_err")
+    return p, p_err
+end
+
+p_filename(M) = "p_fits_M=$M.dat"
+
+function compute_p()
+    @time for M in M_range
+        p_list = find_p.(M, G_range; nsamples=100)
+        writedlm(p_filename(M), [G_range [p[1] for p in p_list] [p[2] for p in p_list]])
+    end
+end
+
+function plot_p_fits()
+    for M in M_range
+        data = readdlm(p_filename(M))
+        G_range, ps, p_errs = data[:,1], data[:,2], data[:,3]
+        errorbar(G_range, ps, yerr=p_errs, capsize=3.0, fmt="-x", label="M = $M")
+    end
+    axhline(-3, label="matter", color="black", ls="--")
+    axhline(-4, label="radiation", color="black", ls=":")
+    xscale("log")
+    xlabel("G")
+    ylabel("p")
+    legend()
+end
+
 
 ######################################### run all the things #####################################
 function main()
-    # TODO: fix all parameters we want to show and
+    lya_G_dep()
     compute_H_osc()
     plot_H_osc()
+    compute_p()
+    plot_p_fits()
 end
