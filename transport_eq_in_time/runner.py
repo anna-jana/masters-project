e2b8ad997f58f66258450a1d8b4245ac7a6f8540
Janik Riess <j.riess@wtnet.de>
2022-05-13 20:06:20 +0200

    finished try 1 of run scan code

--- transport_eq_in_time/runner.py
+++ transport_eq_in_time/runner.py
@@ -35,13 +35,14 @@ def run(name, f, xss, args, kwargs):
     shape = tuple(map(len, xss))
     nsteps = functools.reduce(operator.mul, shape)
     nres = 4
-    neutral = [np.nan]*nres
+    
+    input_data = itertools.product(*xss)
+    
+    tasks = zip(itertools.count(), input_data, itertools.repeat(args), 
+                itertools.repeat(kwargs), itertools.repeat(f), itertools.repeat(nsteps))
         
     logging.info(f"starting computations for {name} ...")
     with ProcessPoolExecutor() as pool:
-        input_data = itertools.product(*xss)
-        tasks = zip(itertools.count(), input_data, itertools.repeat(args), 
-                    itertools.repeat(kwargs), itertools.repeat(f), itertools.repeat(nsteps))
         data_list = list(pool.map(run_task, tasks))
     logging.info("... computations done")
     
@@ -63,31 +64,35 @@ def run(name, f, xss, args, kwargs):
 
 ######################### realignment ########################
 def f_realignment(H_inf, Gamma_inf, m_a, f_a):
-    return H_inf, Gamma_inf, m_a, 1.0
-    return compute_observables(H_inf, Gamma_inf, (m_a,), f_a, 
-                        axion_motion.realignment_axion_field, 
-                        (1.0, 0.0),
-                        calc_init_time=True)
+    return compute_observables(H_inf, Gamma_inf, (m_a,), f_a,  axion_motion.realignment_axion_field,  (1.0, 0.0), calc_init_time=True)
     
 def run_realignment(N=20):
-    H_inf = []
-    f_a = 4 * 10**np.arange(10, 15+1)
+    f_a_list = 4 * 10**np.arange(10, 15 + 1)
+    H_inf_max = f_a_list[-1]*2*np.pi*1e-5
+    H_inf_list = 10**np.arange(-6., 2, 2) * H_inf_max
     Gamma_inf_list = np.geomspace(1e6, 1e10, N)
     m_a_list = np.geomspace(1e6, 1e10, N + 1)
     run("realignment", f_realignment, [H_inf_list, Gamma_inf_list, m_a_list, f_a_list], [], dict())
 
 ############################ clockwork ##########################
-def f_clockwork(H_inf, Gamma_inf, m_a, f_a):
-    return compute_observables(H_inf, Gamma_inf, (eps, M), f, clockwork_axion_field, ax0, 
-                                calc_init_time=True)
+def f_clockwork(H_inf, Gamma_inf, mR, m_phi, f_eff):
+    eps = clockwork_axion.calc_eps(mR)
+    f = clockwork_axion.calc_f(f_eff, eps)
+    M = m_phi / eps
+    theta_i = 3*np.pi/4
+    ax0 = (clockwork_axion.theta_to_phi_over_f(theta_i, eps), 0.0)
+    return H_inf / Gamma_inf, mR, m_phi, 1.0
+    return compute_observables(H_inf, Gamma_inf, (eps, M), f, clockwork_axion.clockwork_axion_field, ax0, calc_init_time=True)
 
-def run_clockwork():
+def run_clockwork(N=20):
     m_phi_list = np.geomspace(1e-6, 1e6, N + 1) * 1e-9 # [GeV]
     mR_list = np.linspace(0, 15, N)
-    sim_f_eff = f_eff = 1e13
-    H_inf = 1e8
-    Gamma_inf = H_inf
-        run("realignment", f_realignment, [H_inf_list, Gamma_inf_list, m_a_list, f_a_list], [], dict())
+    f_eff_list = 4 * 10**np.arange(10, 15 + 1)
+    H_inf_max = f_eff_list[-1]*2*np.pi*1e-5
+    H_inf_list = 10**np.arange(-6., 2, 2) * H_inf_max
+    Gamma_inf_list = np.geomspace(1e6, 1e10, N)
+    
+    run("clockwork", f_clockwork, [H_inf_list, Gamma_inf_list, mR_list, m_phi_list, f_eff_list], [], dict())
 
 ############################### test ##########################
 # testing the run code
@@ -102,3 +107,6 @@ def run_test():
     run("test", f_test, [a_list, b_list], ["arg1", "arg2"], dict(option1="option1", option2="option2"))
     
     
+if __name__ == "__main__":
+    run_realignment()
+    run_clockwork()
