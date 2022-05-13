92bdf4c448460bc50569fd897fe5242c49324e78
Anna-Jana Riess <j.riess@wtnet.de>
2022-05-13 20:21:46 +0200

    fixed runner bugs

--- transport_eq_in_time/runner.py
+++ transport_eq_in_time/runner.py
@@ -29,25 +29,25 @@ def run(name, f, xss, args, kwargs):
             break
         i += 1
     outputfile = f"{name}_data{i}.hdf5"
-    
+
     logging.basicConfig(filename=logfile, level=logging.DEBUG)
-    
+
     shape = tuple(map(len, xss))
     nsteps = functools.reduce(operator.mul, shape)
     nres = 4
-    
+
     input_data = itertools.product(*xss)
-    
-    tasks = zip(itertools.count(), input_data, itertools.repeat(args), 
+
+    tasks = zip(itertools.count(), input_data, itertools.repeat(args),
                 itertools.repeat(kwargs), itertools.repeat(f), itertools.repeat(nsteps))
-        
+
     logging.info(f"starting computations for {name} ...")
     with ProcessPoolExecutor() as pool:
         data_list = list(pool.map(run_task, tasks))
     logging.info("... computations done")
-    
+
     data = np.reshape(data_list, shape + (nres,))
-    
+
     logging.info("storing data in hdf5 file %s", outputfile)
     with h5py.File(outputfile, "w") as fh:
         eta = fh.create_dataset("eta", shape, dtype="f")
@@ -59,13 +59,13 @@ def run(name, f, xss, args, kwargs):
         Omega_h_sq[...] = data[..., 2]
         status[...] = data[..., 3].astype("int")
     logging.info("storing output data done")
-    
+
     logging.info("Terminating program.")
 
 ######################### realignment ########################
 def f_realignment(H_inf, Gamma_inf, m_a, f_a):
-    return compute_observables(H_inf, Gamma_inf, (m_a,), f_a,  axion_motion.realignment_axion_field,  (1.0, 0.0), calc_init_time=True)
-    
+    return observables.compute_observables(H_inf, Gamma_inf, (m_a,), f_a,  axion_motion.realignment_axion_field,  (1.0, 0.0), calc_init_time=True)
+
 def run_realignment(N=20):
     f_a_list = 4 * 10**np.arange(10, 15 + 1)
     H_inf_max = f_a_list[-1]*2*np.pi*1e-5
@@ -81,8 +81,7 @@ def f_clockwork(H_inf, Gamma_inf, mR, m_phi, f_eff):
     M = m_phi / eps
     theta_i = 3*np.pi/4
     ax0 = (clockwork_axion.theta_to_phi_over_f(theta_i, eps), 0.0)
-    return H_inf / Gamma_inf, mR, m_phi, 1.0
-    return compute_observables(H_inf, Gamma_inf, (eps, M), f, clockwork_axion.clockwork_axion_field, ax0, calc_init_time=True)
+    return observables.compute_observables(H_inf, Gamma_inf, (eps, M), f, clockwork_axion.clockwork_axion_field, ax0, calc_init_time=True)
 
 def run_clockwork(N=20):
     m_phi_list = np.geomspace(1e-6, 1e6, N + 1) * 1e-9 # [GeV]
@@ -91,22 +90,9 @@ def run_clockwork(N=20):
     H_inf_max = f_eff_list[-1]*2*np.pi*1e-5
     H_inf_list = 10**np.arange(-6., 2, 2) * H_inf_max
     Gamma_inf_list = np.geomspace(1e6, 1e10, N)
-    
+
     run("clockwork", f_clockwork, [H_inf_list, Gamma_inf_list, mR_list, m_phi_list, f_eff_list], [], dict())
 
-############################### test ##########################
-# testing the run code
-def f_test(a, b, arg1, arg2, option1=1, option2=2):
-    assert (arg1, arg2, option1, option2) == ("arg1", "arg2", "option1", "option2"), \
-            (arg1, arg2, option1, option2)
-    return a*b, a+b, min(a,b), 1  
-  
-def run_test():
-    a_list = np.linspace(1, 10, 10)
-    b_list = np.geomspace(100, 1000, 11)
-    run("test", f_test, [a_list, b_list], ["arg1", "arg2"], dict(option1="option1", option2="option2"))
-    
-    
 if __name__ == "__main__":
     run_realignment()
     run_clockwork()
