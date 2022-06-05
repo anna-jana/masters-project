c56cee7b8b51f39a9a826cbdbdb2c8c85d27bc51
Anna-Jana Riess <j.riess@wtnet.de>
2022-06-05 18:52:23 +0200

    parameter space and correct f_a plots for all source vectors

--- transport_eq_in_time/generic_alp.py
+++ transport_eq_in_time/generic_alp.py
@@ -28,13 +28,13 @@ def recompute_dilution(data, f_a, notebook=False):
     progress = tqdm.notebook.tqdm if notebook else tqdm.tqdm
     m_a, Gamma_inf = data["m_a"], data["Gamma_inf"]
     rho_end_rad = data["rho_end_rad"][0, :, :, 0, 0]
-    rho_end_axion = data["rho_end_axion"][0, :, :, 0, 0]     
+    rho_end_axion = data["rho_end_axion"][0, :, :, 0, 0]
     f_a_used = data["f_a"][0]
     dilution = np.zeros(rho_end_axion.shape)
     for i, j in progress(list(itertools.product(range(len(Gamma_inf)), range(len(m_a))))):
-        dilution[i, j] = observables.compute_dilution_factor_from_axion_decay(10.0, 
-                rho_end_rad[i, j], rho_end_axion[i, j] / f_a_used**2 * f_a**2, 
-                (m_a[j],), f_a, realignment_axion_field, False)  
+        dilution[i, j] = observables.compute_dilution_factor_from_axion_decay(10.0,
+                rho_end_rad[i, j], rho_end_axion[i, j] / f_a_used**2 * f_a**2,
+                (m_a[j],), f_a, realignment_axion_field, False)
     return dilution
 
 def compute_correct_curves(version):
@@ -44,7 +44,7 @@ def compute_correct_curves(version):
     m_a = data["m_a"]
     Gamma_inf = data["Gamma_inf"]
     rho_end_rad = data["rho_end_rad"][0, :, :, 0, 0]
-    rho_end_axion = data["rho_end_axion"][0, :, :, 0, 0]     
+    rho_end_axion = data["rho_end_axion"][0, :, :, 0, 0]
     f_a_used = data["f_a"][0]
 
     f_a_list = np.geomspace(1e12, 1e15, 10)
@@ -52,18 +52,18 @@ def compute_correct_curves(version):
 
     for f_a in tqdm.tqdm(f_a_list, position=0):
         eta_B = eta * recompute_dilution(data, f_a)
-        levels = analysis_tools.find_level(np.log10(m_a), np.log10(Gamma_inf), np.log10(np.abs(eta_B) / eta_B_observed))
+        levels = analysis_tools.find_level(np.log10(m_a), np.log10(Gamma_inf), np.log10(np.abs(eta_B) / observables.eta_B_observed))
         correct_asym_curves.append([(10**xs, 10**ys) for xs, ys in levels])
 
     with open(correct_alp_curves_filename, "wb") as fhandle:
         pickle.dump((f_a_list, correct_asym_curves), fhandle)
-        
+
 def compute_example_trajectories(f_a, H_inf, interesting_points, notebook=False, output_filename=None):
     interesting_solutions = []
 
     for m_a, Gamma_inf in (tqdm.notebook.tqdm if notebook else tqdm.tqdm)(interesting_points):
         background_sols, axion_sols, red_chem_pot_sols = \
-            observables.compute_observables(H_inf, Gamma_inf, (m_a,), f_a, realignment_axion_field, 
+            observables.compute_observables(H_inf, Gamma_inf, (m_a,), f_a, realignment_axion_field,
                                 (1, 0), calc_init_time=True, return_evolution=True)
         ts = np.array([], dtype="d")
         sources = np.empty_like(ts)
@@ -78,7 +78,7 @@ def compute_example_trajectories(f_a, H_inf, interesting_points, notebook=False,
             tinfs = np.geomspace(decay_process.t0, decay_process.t0 + tmax_inf, 300)
             taxs = (tinfs - decay_process.t0) / conv_factor
             plot_ts = tstart + tinfs
-            tstart += tmax_inf 
+            tstart += tmax_inf
 
             Ts, Hs, Tdots = T_and_H_and_T_dot_fn(tinfs)
             theta_dots = axion_sol.sol(taxs)[1, :]
@@ -86,7 +86,7 @@ def compute_example_trajectories(f_a, H_inf, interesting_points, notebook=False,
 
             gammas = [transport_equation.calc_rate_vector(T) for T in Ts]
             source = theta_dots * m_a / Ts # m_a for unit conversion
-            rate = - np.array([gamma @ transport_equation.charge_vector @ transport_equation.charge_vector_B_minus_L 
+            rate = - np.array([gamma @ transport_equation.charge_vector @ transport_equation.charge_vector_B_minus_L
                                             for gamma in gammas]) / Hs
             ts = np.hstack([ts, plot_ts])
             sources = np.hstack([sources, source])
@@ -94,9 +94,9 @@ def compute_example_trajectories(f_a, H_inf, interesting_points, notebook=False,
             red_chem_potss = np.hstack([red_chem_potss, red_chem_pots])
 
         interesting_solutions.append((conv_factor, ts, sources, rates, red_chem_potss))
-    
+
     if output_filename is not None:
         with open(output_filename, "bw") as fh:
             pickle.dump(interesting_solutions, fh)
-    
+
     return interesting_solutions
