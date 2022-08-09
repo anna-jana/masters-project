526fa86cab1c899646dbb6c742e6ed9343effce5
Anna-Jana Riess <j.riess@wtnet.de>
2022-08-09 17:50:56 +0200

    calculcations notebook

--- clockwork_integral.py
+++
@@ -1,7 +0,0 @@
-import sympy as sp
-R, m, y, phi = sp.symbols("R, m, y, phi")
-
-I = 4 * sp.exp(2*m*y) / (1 + sp.exp(2*m*y) * phi)**2
-ans = sp.integrate(I, (y, 0, sp.pi*R))
-ans = ans.simplify()
-print(ans)
