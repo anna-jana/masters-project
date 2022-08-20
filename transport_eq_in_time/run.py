e0c9f1fde60564b4c6dc61980d0f40110cbfedea
Anna-Jana Riess <j.riess@wtnet.de>
2022-08-20 19:09:01 +0200

    first version of the introduction

--- transport_eq_in_time/run.py
+++ transport_eq_in_time/run.py
@@ -1,13 +1,13 @@
 import runner, generic_alp, clockwork_axion
 
-# runner.run_generic_alp(0)
-# runner.run_generic_alp(1, m_a_min=1e5)
-# runner.run_generic_alp(2)
-# for n in range(3)
-#     generic_alp.compute_correct_curves(n + 1)
-# generic_alp.recompute_all_dilutions()
-# generic_alp.compute_all_example_trajectories()
+runner.run_generic_alp(0)
+runner.run_generic_alp(1, m_a_min=1e5)
+runner.run_generic_alp(2)
+for n in range(3):
+    generic_alp.compute_correct_curves(n + 1)
+generic_alp.recompute_all_dilutions()
+generic_alp.compute_all_example_trajectories()
 
-# runner.run_cw_mR_vs_mphi(n)
-# runner.run_cw_Gammainf_vs_mphi()
+runner.run_cw_mR_vs_mphi(n)
+runner.run_cw_Gammainf_vs_mphi()
 clockwork_axion.compute_all_example_trajectories()
