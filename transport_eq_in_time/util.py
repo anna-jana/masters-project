82317b9a9d1cf7e4deb512f3b9b087fff344b13c
Anna-Jana Riess <j.riess@wtnet.de>
2022-08-30 21:02:22 +0200

    Revert "working on making the code readable"
    This reverts commit 38c4a3d19c5b11bfd13d9c2f8dd8d3d0cd1755b4.

--- transport_eq_in_time/util.py
+++ transport_eq_in_time/util.py
@@ -3,6 +3,12 @@ import pickle, os
 
 datadir = "data"
 
+def load_data(name, version):
+    filename = os.path.join(datadir, f"{name}{version}.hdf5")
+    with h5py.File(filename, "r") as fh:
+        data = {key : fh[key][...] for key in fh}
+    return data
+
 def load_pkl(filename):
     with open(filename, "rb") as fh:
         data = pickle.load(fh)
@@ -10,5 +16,5 @@ def load_pkl(filename):
 
 def save_pkl(data, filename):
     with open(filename, "wb") as fh:
-        pickle.dump(data, fh
-        )
+        pickle.dump(data, fh)
+
